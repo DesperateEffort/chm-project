5f74de372064a69decb67bd2bb8cd3f731aed025
CHM <projectlaravel@gmail.com>
2020-03-30 02:58:51 +0800

    upd migration users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ class CreateUsersTable extends Migration
             $table->string('contact');
             $table->string('age');
             $table->string('gender');
-            $table->string('profession')->after('gender');
+            $table->string('profession');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
